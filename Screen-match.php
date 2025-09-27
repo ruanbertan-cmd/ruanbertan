e6e0358aa660f3b3c244194843ab93059f3fd12e
Ruan bertan <ruan.bertan@gmail.com>
2025-09-27 11:22:19 -0300

    feat: estudando principios do php para estruturacao do projeto

--- Screen-match.php
+++ Screen-match.php
@@ -1,11 +1,42 @@
 <?php
 
-echo "Bem vindo ao servidor de filmes!
-";
+echo "Bem vindo ao servidor de filmes!\n\n";
 
 $nomeFilme = "O renascimento - Wolverine";
-$anoLancamento = 2017;
-$notaFilme = 8.5;
-$incluidoNoPlano = true;
+$nomeFilme = "Documentario - David Goggins";
+$nomeFilme = "Se Beber Não Case";
 
-echo $nomeFilme;
+$anoLancamento = $argv[1] ?? 2025;
+$anoAtual = date("Y");
+
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < $anoAtual;
+
+echo "Nome do Filme: $nomeFilme\n";
+echo "Nota do Filme: $notaFilme\n";
+echo "Ano de Lançamento: $anoLancamento\n";
+
+if ($anoLancamento > $anoAtual) {      
+   echo "Esse filme é lançamento!\n";
+} elseif ($anoLancamento > $anoAtual -3 && $anoLancamento <= $anoAtual) {
+   echo "Esse filme é recente!\n";
+} else {
+   echo "Não é um lançamento\n";
+}
+
+$genero = match ($nomeFilme) {
+    "O renascimento - Wolverine" => "Ação",
+    "Documentario - David Goggins" => "Motivação",
+    "Se Beber Não Case" => "Comédia",
+    default => "indefinido",
+};
+
+echo "Gênero: $genero\n";
