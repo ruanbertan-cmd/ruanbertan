403bdf0ef1701c63897c07bcb535f504b1a3606f
Ruan Bertan <ruan.bertan@gmail.com>
2025-12-04 21:54:58 -0300

    Aprendendo sobre classes abstratas

--- screen-match/src/Modelo/Titulo.php
+++ screen-match/src/Modelo/Titulo.php
@@ -1,6 +1,6 @@
 <?php
 
-class Titulo
+abstract class Titulo
 {
     private array $notas;
 
@@ -25,8 +25,5 @@ class Titulo
         return $somaNotas / $quantidadeNotas;
     }
 
-    public function duracaoEmMinutos(): int
-    {
-        return 0;
-    }
+    abstract public function duracaoEmMinutos(): int;
 }
