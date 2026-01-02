07d0e4875e8eaa5abd441318f798061d8d451d1f
Ruan Bertan <ruan.bertan@gmail.com>
2026-01-02 15:29:08 -0300

    manipulação de strings

---
+++ aula-strings/string.php
@@ -0,0 +1,8 @@
+<?php
+
+$string = 'Teste de Integração com PHP';
+
+echo mb_strlen($string) . PHP_EOL;
+echo mb_strtoupper($string);
+
+/* funções com MB servem para textos com caracteres especiais (ç, á ....)*/
