af6da490a5e202ccffe3985f81781ab50f7e0975
Ruan Bertan <ruan.bertan@gmail.com>
2026-01-02 18:56:15 -0300

    funcionalidades strings

--- aula-strings/string.php
+++ aula-strings/string.php
@@ -1,8 +1,8 @@
 <?php
 
-$string = 'Teste de Integração com PHP';
+$string = '.,Teste de Integração com PHP,...';
 
 echo mb_strlen($string) . PHP_EOL;
-echo mb_strtoupper($string);
+echo trim(mb_strtoupper($string), ",.");
 
 /* funções com MB servem para textos com caracteres especiais (ç, á ....)*/
